e5a672ef63a935af1e2a9fdd05d8e14188a255ad
joseliseras <jliseras@gmail.com>
2024-06-19 07:53:38 -0400

    dia_6

---
+++ dia_6.py
@@ -0,0 +1,7 @@
+
+celsius = float(input("Introduce la temperatura en grados Celsius: "))
+
+
+fahrenheit = (celsius * 9/5) + 32
+
+print(f"{celsius} grados Celsius son {fahrenheit} grados Fahrenheit.")
