3d222b3f3850cf32b849f305e27ae1697e16b689
joseliseras <jliseras@gmail.com>
2024-06-24 23:15:09 -0400

    dia_10

---
+++ dia_10.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+
+void bubbleSort(std::vector<int>& arr) {
+    int n = arr.size();
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Intercambiar elementos
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void printArray(const std::vector<int>& arr) {
+    for (int num : arr) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+}
+
+int main() {
+    std::vector<int> arr = {64, 34, 25, 12, 22, 11, 90};
+    
+    std::cout << "Array original: ";
+    printArray(arr);
+    
+    bubbleSort(arr);
+    
+    std::cout << "Array ordenado: ";
+    printArray(arr);
+    
+    return 0;
+}
